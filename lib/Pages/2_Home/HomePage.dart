40a9ccaba14521b10913915493f89cca1acb1618
Dipayan Sarkar <77672753+dipayansarkar47@users.noreply.github.com>
2022-01-25 00:42:49 +0530

    ontap added for promo card and dark theme

--- lib/Pages/2_Home/HomePage.dart
+++ lib/Pages/2_Home/HomePage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hostel_management_app/Pages/SearchPage.dart';
 import 'package:velocity_x/src/extensions/string_ext.dart';
 import 'package:velocity_x/src/flutter/padding.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,29 +12,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _switchValue = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TopBar(),
-              SearchInput(),
-              PromoCard(),
-              Headline(),
-              CardListView(),
-              SHeadline(),
-              LikeListTile(
-                  title: "PG Hostels",
-                  likes: "4.8",
-                  subtitle: "Good environment"),
-              LikeListTile1(
-                  title: "Double Bed",
-                  likes: "5.0",
-                  subtitle: "The service was good"),
-            ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[100],
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const TopBar(),
+                const SearchInput(),
+                const PromoCard(),
+                const Headline(),
+                const CardListView(),
+                const SHeadline(),
+                const LikeListTile(
+                    title: "PG Hostels",
+                    likes: "4.8",
+                    subtitle: "Good environment"),
+                const LikeListTile1(
+                    title: "Double Bed",
+                    likes: "5.0",
+                    subtitle: "The service was good"),
+              ],
+            ),
           ),
         ),
       ),
@@ -57,7 +63,6 @@ class TopBar extends StatelessWidget {
                 color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
           ),
           Container(
-            
             decoration: BoxDecoration(boxShadow: [
               BoxShadow(
                   offset: const Offset(12, 26),
@@ -65,13 +70,32 @@ class TopBar extends StatelessWidget {
                   spreadRadius: 0,
                   color: Colors.grey.withOpacity(.25)),
             ]),
-            child: const CircleAvatar(
-              radius: 25,
-              backgroundColor: Colors.white,
-              child: Icon(
-                Icons.home_outlined,
-                size: 35,
-                color: Colors.orangeAccent,
+            child: GestureDetector(
+              onTap: () {
+                AlertDialog alert = AlertDialog(
+                  title: const Text('Dark Mode is coming soon!'),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () =>
+                            Navigator.of(context, rootNavigator: true).pop(),
+                        child: const Text('OK')),
+                  ],
+                );
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return alert;
+                  },
+                );
+              },
+              child: const CircleAvatar(
+                radius: 25,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.dark_mode,
+                  size: 35,
+                  color: Colors.orangeAccent,
+                ),
               ),
             ),
           )
@@ -134,57 +158,63 @@ class PromoCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(25.0),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 150,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            gradient: const LinearGradient(
-                colors: [Colors.orange, Colors.redAccent])),
-        child: Stack(
-          children: [
-            Image.asset("images/card.png"),
-            const Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: EdgeInsets.all(25.0),
-                child: Text(
-                  "Search your nearest Hostel",
-                  style: TextStyle(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => const Search()));
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 150,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              gradient: const LinearGradient(
+                  colors: [Colors.orange, Colors.redAccent])),
+          child: Stack(
+            children: [
+              Image.asset("images/card.png"),
+              const Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Text(
+                    "Search your nearest Hostel",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Text(
+                    "SALE: 50% off",
+                    style: TextStyle(
                       color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.all(25.0),
-                child: Text(
-                  "SALE: 50% off",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: EdgeInsets.all(25.0),
-                child: Text(
-                  "T&C applied",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
+              const Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Text(
+                    "T&C applied",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -197,7 +227,7 @@ class Headline extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: 25.0, right: 25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -236,21 +266,21 @@ class SHeadline extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: 25.0, right: 25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                 "Popular Hostels",
                 style: TextStyle(
                     color: Colors.black,
                     fontSize: 18,
                     fontWeight: FontWeight.normal),
               ),
-              Text(
+              const Text(
                 "There are some trending hostels",
                 style: TextStyle(
                     color: Colors.grey,
@@ -259,9 +289,10 @@ class SHeadline extends StatelessWidget {
               ),
             ],
           ),
-          Text(
+          const Text(
             "View More",
-            style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold),
+            style: const TextStyle(
+                color: Colors.orange, fontWeight: FontWeight.bold),
           ),
         ],
       ),
@@ -321,26 +352,26 @@ class Card extends StatelessWidget {
         child: Column(
           children: [
             Image.asset(imageUrl, height: 70, fit: BoxFit.cover),
-            Spacer(),
+            const Spacer(),
             Text(text,
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.bold,
                   fontSize: 18,
                 )),
-            SizedBox(
+            const SizedBox(
               height: 5,
             ),
             Text(
               subtitle,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                   color: Colors.grey,
                   fontWeight: FontWeight.normal,
                   fontSize: 12),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
           ],
@@ -383,23 +414,24 @@ class SocialPictureGroup extends StatelessWidget {
                   imgUrl,
                   fit: BoxFit.fitWidth,
                 ),
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(22)),
                   // color: Colors.red
                 ),
                 clipBehavior: Clip.antiAlias,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               Text(
                 title,
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
             ],
           ),
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
         Container(
@@ -430,13 +462,13 @@ class LikeListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      contentPadding: EdgeInsets.all(20),
+      contentPadding: const EdgeInsets.all(20),
       leading: Container(
         width: 50,
         child: AspectRatio(
           aspectRatio: 1,
           child: Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 image: DecorationImage(
                     image: NetworkImage(
@@ -447,16 +479,16 @@ class LikeListTile extends StatelessWidget {
       title: Text(title),
       subtitle: Row(
         children: [
-          Icon(Icons.favorite, color: Colors.orange, size: 15),
-          SizedBox(width: 2),
+          const Icon(Icons.favorite, color: Colors.orange, size: 15),
+          const SizedBox(width: 2),
           Text(likes),
           Container(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 color: Colors.grey,
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
                 child: SizedBox(width: 4, height: 4),
               )),
           Text(subtitle)
@@ -511,13 +543,13 @@ class LikeListTile1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      contentPadding: EdgeInsets.all(20),
+      contentPadding: const EdgeInsets.all(20),
       leading: Container(
         width: 50,
         child: AspectRatio(
           aspectRatio: 1,
           child: Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 image: DecorationImage(
                     image: NetworkImage(
@@ -528,17 +560,17 @@ class LikeListTile1 extends StatelessWidget {
       title: Text(title),
       subtitle: Row(
         children: [
-          Icon(Icons.favorite, color: Colors.orange, size: 15),
-          SizedBox(width: 2),
+          const Icon(Icons.favorite, color: Colors.orange, size: 15),
+          const SizedBox(width: 2),
           Text(likes),
           Container(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 color: Colors.grey,
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: SizedBox(width: 4, height: 4),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: const SizedBox(width: 4, height: 4),
               )),
           Text(subtitle)
         ],
